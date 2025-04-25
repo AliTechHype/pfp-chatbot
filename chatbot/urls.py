71304a6302869528ca05bbb77d851307db1462e7
AliTechHype <157602094+AliTechHype@users.noreply.github.com>
2025-04-25 10:36:39 +0500

    updations of friday

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.chat_view, name='chat'),
 
+    path('', views.chat_view, name='chat'),
     path('api/chat/', views.chat_api, name='chat_api'),
 ]
