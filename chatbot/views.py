69b6e7cb5334db8dd5df5d64a6aca953a2742a70
AliTechHype <157602094+AliTechHype@users.noreply.github.com>
2025-04-28 11:10:40 +0500

    updations on monday

--- chatbot/views.py
+++ chatbot/views.py
@@ -65,6 +65,7 @@
 #     return JsonResponse({'error': 'Invalid request method'}, status=405)
 
 from django.shortcuts import render
+
 from django.http import JsonResponse
 import json
 from .chatbot import get_answer
